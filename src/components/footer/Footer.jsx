391f748c2ec5b26346bb5ff6f06ce77280a7a7a3
shaadq <shaadqureshi8655@gmail.com>
2024-04-28 02:17:17 +0530

    footer added and 2 more sections done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,111 @@
 import React from "react";
+import "./Footer.scss";
+import {
+  FacebookIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  Logo,
+  MailIcon,
+  PhoneIcon,
+  TwitterIcon,
+} from "../../assets/img/svg";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            
+      <div className="bg-blue py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="logo-wrapper mb-4">
+                <Logo />
+              </div>
+              <div className="text-light mb-3">
+                Boozno is your all-in-one solution for <br />
+                venue management and experience unparalleled convenience.
+              </div>
+              <div className="footer-socialmedia-items mb-4">
+                <div className="item">
+                  <FacebookIcon />
+                </div>
+                <div className="item">
+                  <TwitterIcon />
+                </div>
+                <div className="item">
+                  <InstagramIcon />
+                </div>
+                <div className="item">
+                  <LinkedinIcon />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="row">
+                <div className="text-white footer-link-title">
+                  Usefull Links
+                </div>
+                <div className="col-6">
+                  <div className="footer-links text-light">
+                    <div className="item">Home</div>
+                    <div className="item">Solutions</div>
+                    <div className="item">Pricing Plan</div>
+                    <div className="item">About Us</div>
+                    <div className="item">Career</div>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="footer-links text-light">
+                    <div className="item">Blogs</div>
+                    <div className="item">Talk to Sales</div>
+                    <div className="item">Schedule Demo</div>
+                    <div className="item">Contact Us</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-4">
+              <div className="row">
+                <div className="col-6">
+                  <div className="text-white footer-link-title">Legal</div>
+                  <div className="footer-links text-light">
+                    <div className="item">Terms of Services</div>
+                    <div className="item">Privacy Policy</div>
+                    <div className="item">FAQ's</div>
+                    <div className="item">Cookie Policy</div>
+                    <div className="item">Do Not Sell My Personal Info</div>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="text-white footer-link-title">Support</div>
+                  <div className="footer-links text-light">
+                    <div className="d-flex align-items-center item">
+                      <MailIcon />
+                      <div className="ms-2">help@boozno.com</div>
+                    </div>
+                    <div className="d-flex align-items-center item">
+                      <PhoneIcon />
+                      <div className="ms-2">+91 12345 54321</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div>
+                <div className="text-white mb-3 fs-18">
+                  Subscribe to our weekly email newsletter
+                </div>
+                <div className="footer-subscribe-wrapper">
+                  <input type="text" placeholder="Add your email address" />
+                  <button className="btn btn-primary">Subscribe</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="bg-dark-blue py-3 text-white text-center">
+        © 2023 Boozno Technologies Pvt. Ltd. All rights reserved.{" "}
       </div>
     </footer>
   );
--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -5,9 +5,60 @@ import {
   NoCardIcon,
   PlayerIcon,
   SandClockIcon,
+  TopRightArrowIcon,
+  TopRightArrowIconWhite,
 } from "../../../assets/img/svg";
+import img1 from "../../../assets/img/sales-1.png";
 
 const Homepage = () => {
+  let salesList = [
+    {
+      img: img1,
+      title: "Dashboard",
+      desc: "Empower decision-making with a dynamic dashboard showcasing real-time data & analytics for bookings, revenue and more",
+    },
+    {
+      img: img1,
+      title: "Availability Calendar",
+      desc: "Stay organised with a real-time, integrated calendar with all the data to stay organised",
+    },
+    {
+      img: img1,
+      title: "Inquiries",
+      desc: "Streamline inquiries with creation, qualification, tracking, and effective duplication management",
+    },
+    {
+      img: img1,
+      title: "Bookings",
+      desc: "Effortlessly handle bookings, alerts, modifications, and cancellations for optimal efficiency",
+    },
+    {
+      img: img1,
+      title: "Reports & Analytics",
+      desc: "Understand your business better with detailed reports and useful analytics based on historical data",
+    },
+    {
+      img: img1,
+      title: "Alerts, Notification & Reminders",
+      desc: "Stay in the loop with instant updates through emails, text messages, and system alerts",
+    },
+    {
+      img: img1,
+      title: "Customer Management",
+      desc: "Create outstanding guest experiences with detailed customer profiles and booking history",
+    },
+
+    {
+      img: img1,
+      title: "Venue Onboarding",
+      desc: "Easily set up your venue with a straightforward onboarding process",
+    },
+    {
+      img: img1,
+      title: "Lead Management",
+      desc: "Maximize sales potential with seamless lead capturing, qualification, and nurturing.",
+    },
+  ];
   return (
     <div className="margin-top-minus88">
       <section className="bg-purple-img">
@@ -39,7 +90,9 @@ const Homepage = () => {
               </div>
               <div className="d-flex me-4">
                 <HeadphoneSolidIcon />
-                <div className="ms-2 whitespace-nowrap">Dedicated venue experts</div>
+                <div className="ms-2 whitespace-nowrap">
+                  Dedicated venue experts
+                </div>
               </div>
             </div>
 
@@ -50,6 +103,110 @@ const Homepage = () => {
           </div>
         </div>
       </section>
+
+      {/* Collection of Sales Section Start */}
+      <section className="py-5">
+        <div className="container">
+          <h2 className="section-title text-dark-blue mb-3">
+            A collection of sales tools designed <br /> with revenue growth in
+            mind
+          </h2>
+          <div className="section-subtitle">
+            End-To-End, Powerful Suite of Features for efficient leads and
+            bookings management
+          </div>
+          <div className="row mt-5">
+            {salesList.map((data, index) => (
+              <div className="col-md-4 mb-3" key={index}>
+                <div className="collection-sales-card">
+                  <img src={data.img} className="mb-3" alt="" />
+                  <h5>{data.title}</h5>
+                  <p>{data.desc}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      {/* Collection of Sales Section End */}
+
+      {/* View Plans Section Start */}
+      <section className="py-5">
+        <div className="container">
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <h2 className="section-title text-center text-md-start">
+                Lorem Ipsum dolor sit amet
+              </h2>
+            </div>
+            <div className="col-md-6">
+              <div className="section-subtitle text-center text-md-end">
+                We are a team of 20+ who are passionate about <br />
+                making the world a better place.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="view-plan-bg-img">
+                <div>
+                  <div className="text-white">
+                    Dive into our blog posts for useful tips and inspiration
+                  </div>
+                  <h3 className="text-white">Read our blogs</h3>
+                </div>
+                <div>
+                  <button className="btn btn-primary view-plan-btn">
+                    <div className="me-3">View Details</div> <TopRightArrowIconWhite />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="homepage-plan-card bg-beige mb-4">
+                    <div className="mb-5">
+                      Find the best plan for your{" "}
+                      <br className="d-none d-md-block" /> venue management
+                    </div>
+                    <div className="view-plan-wrapper">
+                      <h6>View our Plans </h6>
+                      <TopRightArrowIcon />
+                    </div>
+                  </div>
+
+                  <div className="homepage-plan-card bg-light-blue">
+                    <div className="mb-5">
+                      Get personalized advice by{" "}
+                      <br className="d-none d-md-block" /> our venue experts for
+                      tailored <br className="d-none d-md-block" /> insights
+                    </div>
+                    <div className="view-plan-wrapper">
+                      <h6>Talk to our Sales </h6>
+                      <TopRightArrowIcon />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="homepage-plan-card bg-purple h-100">
+                    <div className="mb-5">
+                      See innovation in action —{" "}
+                      <br className="d-none d-md-block" /> book your own
+                      software <br className="d-none d-md-block" /> demo today
+                    </div>
+                    <div className="view-plan-wrapper">
+                      <h6>Book a Demo</h6>
+                      <TopRightArrowIcon />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* View Plans Section End */}
     </div>
   );
 };
