d09013ed9a7c21b453b9b86d289fbdbf500de4bc
shaad.q <shaad.q@intelegain.com>
2024-05-02 18:45:32 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Homepage from "./components/pages/homepage/Homepage";
-import ProductPage from "./components/pages/products/ProductPage";
 import PricingPage from "./components/pages/pricing/PricingPage";
-import { useEffect } from "react";
+import ProductPage from "./components/pages/products/ProductPage";
+import ScrollToTop from "./utils/ScrollToTop";
+import TitleUpdater from "./utils/TitleUpdater";
 
-const TitleUpdater = () => {
-  const location = useLocation();
-  useEffect(() => {
-    switch (location.pathname) {
-      case "/":
-        document.title = "Boozno - Homepage";
-        break;
-      case "/solution":
-        document.title = "Boozno - Solutions";
-        break;
-      case "/pricing":
-        document.title = "Boozno - Pricing";
-        break;
-      case "/blog":
-        document.title = "Boozno - Blog";
-        break;
-      default:
-        document.title = "My App";
-    }
-  }, [location]);
-};
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <TitleUpdater />
       <Layout>
         <Routes>
---
+++ src/utils/TitleUpdater.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const TitleUpdater = () => {
+  const location = useLocation();
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        document.title = "Boozno - Homepage";
+        break;
+      case "/solution":
+        document.title = "Boozno - Solutions";
+        break;
+      case "/pricing":
+        document.title = "Boozno - Pricing";
+        break;
+      case "/blog":
+        document.title = "Boozno - Blog";
+        break;
+      default:
+        document.title = "My App";
+    }
+  }, [location]);
+  return null;
+};
+
+export default TitleUpdater;
